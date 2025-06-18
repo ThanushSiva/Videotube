8b7847144dafcbc25434490e7a75fd073fa299b3
ThanushSiva <thanushsiva06@gmail.com>
2025-06-18 22:22:13 +0530

    initializing project with db

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const DB_NAME = require("../constants.js");
+
+const connectDB = async () => {
+  try {
+    const connecttionInstance = await mongoose.connect(
+      `${process.env.DB_URL}/${DB_NAME}`
+    );
+    console.log(`MongoDB connected`);
+  } catch (error) {
+    console.log("MongoDB connection error : ", error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv");
+const connectDB = require("./db/index");
+dotenv.config({ path: "./src/.env" });
+
+const app = require("./app");
+
+const PORT = process.env.PORT;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT || 7001, () => {
+      console.log(`server is running in port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MongoDB connection error`);
+  });
