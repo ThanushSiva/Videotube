f5beeb011e1e7a21323adf52bbe25c94846c4a73
thanush <thanushsiva06@gmail.com>
2025-06-26 23:13:34 +0530

    added multer middleware and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+const { v2 } = require("cloudinary");
+const fs = require("fs");
+
+v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await v2.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("file url : ", response.url);
+    fs.unlinkSync(localFilePath);
+    return response.url;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+module.exports = uploadOnCloudinary;
